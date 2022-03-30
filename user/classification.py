2085285128b7a56d687bb0028a7f1615d5836be6
HarshSonawane <sonawanehvs@gmail.com>
2022-03-31 00:34:26 +0530

    test mechanism updated

--- user/classification.py
+++ user/classification.py
@@ -150,27 +150,42 @@ def identify_images(image_path):
     import numpy as np
     import matplotlib.pyplot as plt
 
-    # load json and create model
-    json_file = open(base_dir + 'VGG16_pre_train_model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights(base_dir + 'VGG16_pre_train_weights.h5')
-    print("Loaded model from disk")
+    # # load json and create model
+    # json_file = open(base_dir + 'VGG16_pre_train_model.json', 'r')
+    # loaded_model_json = json_file.read()
+    # json_file.close()
+    # loaded_model = model_from_json(loaded_model_json)
+    # # load weights into new model
+    # loaded_model.load_weights(base_dir + 'VGG16_pre_train_weights.h5')
+    # print("Loaded model from disk")
 
-    # evaluate loaded model on test data
-    loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['categorical_accuracy'])
-    #img = image.load_img(image_path, target_size=(224, 224))
+    # # evaluate loaded model on test data
+    # loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['categorical_accuracy'])
+    # #img = image.load_img(image_path, target_size=(224, 224))
+    # img = image.load_img(image_path, target_size=(224, 224))
+    # x = image.img_to_array(img)
+    # x = np.expand_dims(x, axis=0)
+    # x = preprocess_input(x)
+    # preds = loaded_model.predict(x)
+    # # decode the results into a list of tuples (class, description, probability)
+    # # (one such list for each sample in the batch)
+    # print('Predicted:', decode_predictions(preds, top=3)[0])
+    # # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+
+    base_dir = os.path.join(BASE_DIR, 'static/data/')
+    model = base_dir + 'VGG16_pre_train_model.h5'
+    model = load_model(model)
+
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    preds = loaded_model.predict(x)
+    preds = model.predict(x)
     # decode the results into a list of tuples (class, description, probability)
     # (one such list for each sample in the batch)
     print('Predicted:', decode_predictions(preds, top=3)[0])
     # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+    
 
 
 train_model()
