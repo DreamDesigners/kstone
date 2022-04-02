9b96a991006c57245a95697dfe7fc6a87d1703d5
HarshSonawane <sonawanehvs@gmail.com>
2022-04-02 16:01:13 +0530

    update

--- tnx_1.py
+++ tnx_1.py
@@ -1,45 +1,241 @@
-import re
-import os
 from pathlib import Path
 import os
-from tkinter.filedialog import test
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+import os
+import re
 import numpy as np
-import pandas as pd
-import tensorflow as tf
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import pandas as pd
+%matplotlib inline
+import seaborn as sns
+sns.set_style("whitegrid")
+import warnings
+warnings.filterwarnings("ignore")
+import tensorflow as tf
+import glob
+from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
 
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
-    print('Device:', tpu.master())
-    tf.config.experimental_connect_to_cluster(tpu)
-    tf.tpu.experimental.initialize_tpu_system(tpu)
-    strategy = tf.distribute.experimental.TPUStrategy(tpu)
-except:
-    strategy = tf.distribute.get_strategy()
-print('Number of replicas:', strategy.num_replicas_in_sync)
+
+train_dir = BASE_DIR + 'static/data/train'
+test_dir = BASE_DIR + 'static/data/test'
+val_dir = BASE_DIR + 'static/data/val'
+
+print("Train set:")
+print("-"*60)
+num_pneumonia = len(os.listdir(os.path.join(train_dir, 'PNEUMONIA')))
+num_normal = len(os.listdir(os.path.join(train_dir, 'NORMAL')))
+print(f"PNEUMONIA={num_pneumonia}")
+print(f"NORMAL={num_normal}")
+
+print("\nTest set:")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(test_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(test_dir, 'NORMAL')))}")
+
+print("\nValidation set")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(val_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(val_dir, 'NORMAL')))}")
+
+pneumonia = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA")
+pneumonia_dir = "../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA"
+
+
+
+plt.figure(figsize=(15, 5))
+
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    img = plt.imread(os.path.join(pneumonia_dir, pneumonia[i]))
+    plt.title("PNEUMONIA")
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
     
-print(tf.__version__)
+
+plt.tight_layout()
 
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 16 * strategy.num_replicas_in_sync
-IMAGE_SIZE = [180, 180]
-EPOCHS = 2
 
 
-base_dir = os.path.join(BASE_DIR, 'static/data/')
 
-train_path = base_dir + 'train/'
-test_path = base_dir + 'test/'
-valid_path = base_dir + 'val/'
 
-filenames = os.listdir(train_path)
-filenames = [os.path.join(train_path, f) for f in filenames]
 
-COUNT_NORMAL = os.listdir(train_path + 'Normal/')
-COUNT_NORMAL = [os.path.join(train_path + 'Normal/', f) for f in COUNT_NORMAL]
-COUNT_STONE = os.listdir(train_path + 'Stone/')
-COUNT_STONE = [os.path.join(train_path + 'Stone/', f) for f in COUNT_STONE]
+normal = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/NORMAL")
+normal_dir = "../input/chest-xray-pneumonia/chest_xray/train/NORMAL"
+
+plt.figure(figsize=(10, 5))
+
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    img = plt.imread(os.path.join(pneumonia_dir, pneumonia[i]))
+    plt.title("NORMAL")
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+    
+plt.tight_layout()
+
+
+import glob
+
+pneumonia_train = glob.glob(train_dir+"/PNEUMONIA/*.jpeg")
+normal_train = glob.glob(train_dir+"/NORMAL/*.jpeg")
+
+
+data = pd.DataFrame(np.concatenate([[0]*len(normal_train) , [1]*len(pneumonia_train)]),columns=["class"])
+
+
+plt.figure(figsize=(15,10))
+sns.countplot(data['class'],data=data,palette='rocket')
+plt.title('PNEUMONIA vs NORMAL')
+plt.show()
+
+img_Datagen = ImageDataGenerator(
+        rescale = 1/255,
+        shear_range=10,
+        zoom_range=0.3,
+        horizontal_flip=True,
+        width_shift_range = 0.2,
+        rotation_range=20,
+        fill_mode = 'nearest'
+)
+val_Datagen = ImageDataGenerator(
+        rescale = 1/255
+)
+
+train = img_Datagen.flow_from_directory(train_dir,
+                                       batch_size=8,
+                                       class_mode='binary',
+#                                        target_size=(224,224,3))
+                                       )
+
+validation = val_Datagen.flow_from_directory(val_dir,
+                                              batch_size=1,
+                                              class_mode='binary',
+#                                               target_size=(224,224,3))
+                                            )
+
+test = img_Datagen.flow_from_directory(test_dir,
+                                       batch_size=1,
+                                       class_mode='binary',
+#                                        target_size=(224/,224,3))
+                                      )
+
+img, label = next(train)
+img.shape
+
+vgg_model = tf.keras.applications.VGG19(
+    weights='imagenet',
+    include_top = False,
+#     input_shape = (224,224,3)
+)
+
+for layer in vgg_model.layers:
+    layer.trainable=False
+    
+x = vgg_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128,activation='relu')(x)
+# output layer
+predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model = tf.keras.Model(inputs=vgg_model.input, outputs=predictions)
+
+# to avoid overfitting
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=6)
+
+# Compiling the model
+model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+
+model.summary()
+
+history = model.fit(train,epochs=30, 
+                    validation_data=validation,
+                     steps_per_epoch=100,
+#                     callbacks=[early_stopping],
+                    batch_size=32)
+
+# Evaluating the model on traina and test
+score = model.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+
+# Test data
+score = model.evaluate(test)
+
+print("Test Loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+
+mobileNet_model = tf.keras.applications.MobileNetV2(
+    weights='imagenet',
+    include_top = False,
+    input_shape = (224,224,3)
+)
+
+for layer in mobileNet_model.layers:
+    layer.trainable=False
+    
+x = mobileNet_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128,activation='relu')(x)
+# output layer
+predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model2 = tf.keras.Model(inputs=mobileNet_model.input, outputs=predictions)
+
+# to avoid overfitting
+# early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=6)
+
+# Compiling the model
+model2.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+history = model2.fit(train,epochs=30, 
+                    validation_data=validation,
+                     steps_per_epoch=100,
+#                     callbacks=[early_stopping],
+                    batch_size=32)
+
+
+# Evaluating the model on traina and test
+score = model2.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model2.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
 
