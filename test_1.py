35ad17565378458a19ba3870c67506e274d77f48
HarshSonawane <sonawaneharshwardhan@gmail.com>
2022-04-03 20:26:37 +0000

    final corrections done

--- test_1.py
+++ test_1.py
@@ -18,20 +18,36 @@ import tensorflow as tf
 import glob
 from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
 
-print("testing")
-model = tf.keras.models.load_model('model.h5')
-model.summary()
+def test(test_image=None):
+        print("testing")
+        model = tf.keras.models.load_model('model.h5')
+        model.summary()
 
 
-# pridict if test_image_path is a stone or normal
+        # pridict if test_image_path is a stone or normal
 
-# load image
-img = load_img(test_image_path, target_size=(150, 150))
-img_array = np.array(img)
-img_array = img_array/255
-img_array = img_array.reshape(1, 150, 150, 3)
+        # load image
+        img = load_img(test_image_path, target_size=(150, 150))
+        img_array = np.array(img)
+        img_array = img_array/255
+        img_array = img_array.reshape(1, 150, 150, 3)
 
 
-# predict
-prediction = model.predict(img_array)
-print(prediction)
+        # predict
+        prediction = model.predict(img_array)
+        print(prediction)
+
+
+        # print accuracy
+
+
+        # print prediction
+        if prediction[0][0] > 0.5:
+                print("Stone")
+        else:
+                print("Normal")
+
+        return prediction[0][0]
+
+
+test(test_image_path)
